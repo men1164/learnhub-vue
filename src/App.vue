21a2aaf482e32af5f0334514cdd4695307812662
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-09 14:34:40 +0700

    comp: navbar and banner

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BannerComp from './components/BannerComp.vue'
+import NavbarComp from './components/NavbarComp.vue'
+</script>
 
 <template>
-  <div>
+  <div class="min-h-screen w-full">
+    <navbar-comp />
+    <banner-comp />
     <router-view />
   </div>
 </template>
---
+++ src/components/BannerComp.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col gap-y-3 px-32 py-24 bg-orange-200/50">
+    <p class="font-bold text-5xl text-orange-500">LearnHub</p>
+    <p class="text-gray-500">Hub for Educational Videos</p>
+  </div>
+</template>
