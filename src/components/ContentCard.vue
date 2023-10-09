6743863500f73ecb0e9ee04e3e6c5f70c65e7e88
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-09 15:26:00 +0700

    feat: homepage

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,16 +1,21 @@
+<script setup lang="ts">
+import { ContentDTO } from '../types/dto'
+
+defineProps<{ content: ContentDTO }>()
+</script>
+
 <template lang="">
   <div class="flex flex-col bg-gray-100 rounded-xl shadow-lg overflow-hidden">
-    <img
-      src="https://i.ytimg.com/vi/7B9f9kNwUIc/hqdefault.jpg"
-      class="w-full aspect-video object-cover"
-    />
+    <img :src="content.thumbnailUrl" class="w-full aspect-video object-cover" />
     <div class="flex flex-col gap-4 p-4 justify-between h-full">
       <div class="flex flex-col gap-2">
-        <p class="font-semibold text-md text-gray-500">Test</p>
-        <p class="text-gray-500">Test</p>
-        <p class="text-gray-500 italic">Test</p>
+        <p class="font-semibold text-md text-gray-500">
+          {{ content.videoTitle }}
+        </p>
+        <p class="text-gray-500">{{ content.creatorName }}</p>
+        <p class="text-gray-500 italic">{{ content.comment }}</p>
       </div>
-      <p class="text-md text-gray-500">Test</p>
+      <p class="text-md text-gray-500">{{ content.postedBy.name }}</p>
     </div>
   </div>
 </template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,27 +1,22 @@
 <script setup lang="ts">
 import BannerComp from '../components/BannerComp.vue'
-import ContentCard from '../components/ContentCard.vue'
+import ContentList from '../components/ContentList.vue'
+import useContents from '../composables/useContents'
+
+const { contents, error } = useContents()
 </script>
 
 <template>
   <BannerComp />
-  <div class="flex flex-col items-end mx-32 my-9">
+  <div v-if="error" class="text-center mt-5 text-red-500 font-bold">
+    {{ error }}
+  </div>
+  <div v-else-if="contents" class="flex flex-col items-end mx-32 my-9">
     <div
       class="bg-orange-500 p-3 rounded-lg text-white hover:bg-orange-600 text-lg"
     >
       Create New Content
     </div>
-    <div
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-stretch justify-stretch my-9 h-auto"
-    >
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-    </div>
+    <ContentList :contents="contents" />
   </div>
 </template>
