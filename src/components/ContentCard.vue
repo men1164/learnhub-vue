f92681c7cb38fadb59e55e794300fcd368361f05
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-09 14:41:17 +0700

    content card ui

---
+++ src/components/ContentCard.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <div class="flex flex-col bg-gray-100 rounded-xl shadow-lg overflow-hidden">
+    <img
+      src="https://i.ytimg.com/vi/7B9f9kNwUIc/hqdefault.jpg"
+      class="w-full aspect-video object-cover"
+    />
+    <div class="flex flex-col gap-4 p-4 justify-between h-full">
+      <div class="flex flex-col gap-2">
+        <p class="font-semibold text-md text-gray-500">Test</p>
+        <p class="text-gray-500">Test</p>
+        <p class="text-gray-500 italic">Test</p>
+      </div>
+      <p class="text-md text-gray-500">Test</p>
+    </div>
+  </div>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BannerComp from '../components/BannerComp.vue'
+import ContentCard from '../components/ContentCard.vue'
+</script>
 
 <template>
-  <div>Home</div>
+  <BannerComp />
+  <div class="flex flex-col items-end mx-32 my-9">
+    <div
+      class="bg-orange-500 p-3 rounded-lg text-white hover:bg-orange-600 text-lg"
+    >
+      Create New Content
+    </div>
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-stretch justify-stretch my-9 h-auto"
+    >
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+    </div>
+  </div>
 </template>
