3b2b652a1bf657e3bd2a07325584bb550a563539
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-09 14:41:05 +0700

    remove banner from app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import BannerComp from './components/BannerComp.vue'
 import NavbarComp from './components/NavbarComp.vue'
 </script>
 
 <template>
   <div class="min-h-screen w-full">
     <navbar-comp />
-    <banner-comp />
     <router-view />
   </div>
 </template>
